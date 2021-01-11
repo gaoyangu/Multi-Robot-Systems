3ce3208f32f728c0449a1c5dc8826dde6c9c65af
gaoyangu <1600314850@qq.com>
2021-01-11 10:36:31 +0800

    creat_pkg: tf_multi

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ $ roslaunch stage_multi stage_multi.launch
 
 ## Thanks
 - [89-689: Multi-Robot Systems](https://u.cs.biu.ac.il/~yehoshr1/89-689/) (Summer 2015)
+- [89-685: Introduction to Robotics](https://u.cs.biu.ac.il/~yehoshr1/89-685/) (Fall 2016)
---
+++ tf_multi/src/print_position.cpp
@@ -0,0 +1,79 @@
+// ros includes 
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+
+//boost includes
+#include <boost/lexical_cast.hpp>
+
+// std includes
+#include <string>
+using namespace std;
+
+string tf_prefix;
+//pair<double, double> getRobotPosition();
+pair<double, double> getRobotPosition(int robot_no);
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "print_position");
+    ros::NodeHandle nh;
+
+    //get tf_prefix from the parameter server
+    nh.getParam("tf_prefix", tf_prefix);
+
+    pair<double, double> currPosition;
+    ros::Rate loopRate(1);
+
+    int team_size = 4;
+
+    while (ros::ok()){
+        for(int i = 0; i < team_size; i++){
+            currPosition = getRobotPosition(i);
+            ROS_INFO("Robot %d position:(%.3f, %.3f)", i, currPosition.first, currPosition.second);
+        }
+        ROS_INFO("----------------");
+        loopRate.sleep();
+    }
+    return 0;
+}
+
+pair<double, double> getRobotPosition(int robot_no)
+{
+    tf::TransformListener listener;
+    tf::StampedTransform transform;
+    pair<double, double> currPosition;
+    try
+    {
+        string robot_str = "/robot_";
+        robot_str += boost::lexical_cast<string>(robot_no);
+        string base_footprint_frame = tf::resolve(robot_str, "base_footprint");
+        listener.waitForTransform("/map", base_footprint_frame, ros::Time(0), ros::Duration(10.0));
+        listener.lookupTransform("/map", base_footprint_frame, ros::Time(0), transform);
+        currPosition.first = transform.getOrigin().x();
+        currPosition.second = transform.getOrigin().y();
+    }
+    catch(tf::TransformException &ex)
+    {
+        ROS_ERROR("%s", ex.what());
+    }
+    return currPosition;
+}
+
+/*pair<double, double> getRobotPosition()
+{
+    tf::TransformListener listener;
+    tf::StampedTransform transform;
+    pair<double, double> currPosition;
+
+    try{
+        string base_footprint_frame = tf::resolve(tf_prefix, "base_footprint");
+        listener.waitForTransform("/map", base_footprint_frame, ros::Time(0), ros::Duration(10.0));
+        listener.lookupTransform("/map", base_footprint_frame, ros::Time(0), transform);
+
+        currPosition.first = transform.getOrigin().x();
+        currPosition.second = transform.getOrigin().y();
+    }catch(tf::TransformException &ex){
+        ROS_ERROR("%s", ex.what());
+    }
+    return currPosition;
+}*/
